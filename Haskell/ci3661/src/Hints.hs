b69df6bf1289dc303b4bdf95f945c671b0694a73
Jesus Bandez <jesusbandezt@gmail.com>
2022-03-08 23:35:29 -0400

    fullmatch implemented

--- Haskell/ci3661/src/Hints.hs
+++ Haskell/ci3661/src/Hints.hs
@@ -18,3 +18,9 @@ match _ (Target "") = []
 match (Guess (g : gs)) (Target (t: ts)) | g == t = Correct g : match (Guess gs) (Target ts)
                                         | g `elem` ts = Misplaced g : match (Guess gs) (Target ts)
                                         | otherwise = Absent g : match (Guess gs) (Target ts)
+
+fullmatch :: [Match] -> Bool
+fullmatch ms = foldr ((&&) . isCorrect) True ms
+            where
+                isCorrect (Correct _) = True
+                isCorrect _ = False

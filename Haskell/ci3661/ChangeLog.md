eadc217b4937529c0d89a89027e4dd4810fdf816
Jesus Bandez <jesusbandezt@gmail.com>
2022-03-25 19:15:00 -0400

    removed ci3661

--- Haskell/ci3661/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for ci3661
-
-## Unreleased changes
--- Haskell/ci3661/wordle/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-
-import Play ( initialState, playTheGame )
-
-
-main :: IO ()
-main = do putStrLn "Kinda Wordle!"
-          gs <- initialState          
-          playTheGame gs
